82622cf31728a9d655df09782a1bbcdfe4af9cf3
SimRoad <montanez.simonemikaela@gmail.com>
2023-09-21 22:29:49 +0800

    Finished version 2

--- EmployeeProject/src/employee/version2/TestMain.java
+++ EmployeeProject/src/employee/version2/TestMain.java
@@ -40,45 +40,31 @@ public class TestMain {
         System.out.println("----------------------Commision Employees:----------------------");
             CommissionEmployee commEmp1 = new CommissionEmployee(1, "Hua Shui", temp, temp);
             CommissionEmployee commEmp2 = new CommissionEmployee(2, "Cain Mk V", temp, temp, 57000);
+            CommissionEmployee commEmp3 = new CommissionEmployee(3, "Ori Verd", temp, temp, 690000);
+            CommissionEmployee commEmp4 = new CommissionEmployee(4, "Maris Mischa", temp, temp, 1000000);
+
             commEmp1.displayInfo();
             commEmp1.setTotalSales(36000);
 
             commEmp1.displayInfo();
             System.out.println("Salary: " + commEmp1.computeSalary() + '\n');
             System.out.println(commEmp2.toString() + "\nSalary: " + commEmp2.computeSalary() + '\n');
+            System.out.println(commEmp3.toString() + "\nSalary: " + commEmp3.computeSalary() + '\n');
+            System.out.println(commEmp4.toString() + "\nSalary: " + commEmp4.computeSalary() + '\n');
+
+        System.out.println("----------------------Commision Employees:----------------------");
+            BasePlusCommissionEmployee baseCommEmp1 = new BasePlusCommissionEmployee(1, "Hua Shui", temp, temp);
+            BasePlusCommissionEmployee baseCommEmp2 = new BasePlusCommissionEmployee(2, "Cain Mk V", temp, temp, 57000, 42);
+            BasePlusCommissionEmployee baseCommEmp3 = new BasePlusCommissionEmployee(3, "Ori Verd", temp, temp, 690000, 42);
+            BasePlusCommissionEmployee baseCommEmp4 = new BasePlusCommissionEmployee(4, "Maris Mischa", temp, temp, 1000000, 42);
+            baseCommEmp1.displayInfo();
+            baseCommEmp1.setTotalSales(36000);
+            baseCommEmp1.setBaseSalary(45);
+
+            baseCommEmp1.displayInfo();
+            System.out.println("Salary: " + baseCommEmp1.computeSalary() + '\n');
+            System.out.println(baseCommEmp2.toString() + "\nSalary: " + baseCommEmp2.computeSalary() + '\n');
+            System.out.println(baseCommEmp3.toString() + "\nSalary: " + baseCommEmp3.computeSalary() + '\n');
+            System.out.println(baseCommEmp4.toString() + "\nSalary: " + baseCommEmp4.computeSalary() + '\n');
     }
 }
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
-//deez nuts
